978c30e61911f45feaa3d40e2fd75566af429755
rt885 <ruben.t.v@hotmail.com>
2023-01-13 17:50:48 -0600

    link pages

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About () {
+    <div>
+        <h1>About WebPage</h1>
+        <p>This is the about web page</p>
+        <p>Try Page</p>
+    </div>
+}
+
+export default About;
